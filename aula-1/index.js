0a01843c584f25f86d1cc565e3092d37c9679fe8
Gabriel Martinez <martinegabriel47@gmail.com>
2023-03-26 12:12:19 -0500

    Create index.js

---
+++ aula-1/index.js
@@ -0,0 +1,11 @@
+{
+    nombreCliente;
+    dniCliente;
+    numeroCuenta;
+    saldoCuenta;
+}
+
+const nombreCliente = "Leonardo";
+const dniCliente = "1053249654";
+const numeroCuenta = "12542369";
+const saldoCuenta = 1000;
