0d59a11393c60c9327bd89f2baf30d615a0701b0
Gabriel Martinez <martinegabriel47@gmail.com>
2023-03-26 12:34:47 -0500

    Update index.js

--- aula-1/index.js
+++ aula-1/index.js
@@ -1,9 +1,19 @@
+class Cliente
 {
     nombreCliente;
     dniCliente;
     numeroCuenta;
     saldoCuenta;
 }
+
+const cliente1 =  new Cliente();
+cliente1.nombreCliente = "Jose";
+cliente1.dniCliente = "5445545";
+cliente1.numeroCuenta = "21843";
+cliente1.saldoCuenta = 3000;
+
+console.log(cliente1);
+
 
 const nombreCliente = "Leonardo";
 const dniCliente = "1053249654";
