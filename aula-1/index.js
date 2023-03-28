4b3926c1c13c5ece54a69ca562c0882f3a7dc37e
Gabriel Martinez <martinegabriel47@gmail.com>
2023-03-28 15:14:27 -0500

    Update index.js

--- aula-1/index.js
+++ aula-1/index.js
@@ -1,9 +1,9 @@
-class Cliente
-{
+class Cliente{
     nombreCliente;
     dniCliente;
     numeroCuenta;
     saldoCuenta;
+    rutCliente;
 }
 
 const cliente1 =  new Cliente();
@@ -14,8 +14,18 @@ cliente1.saldoCuenta = 3000;
 
 console.log(cliente1);
 
+const cliente2 = new Cliente();
+cliente2.nombreCliente = "Leonardo";
+cliente2.dniCliente = "1053249654";
+cliente2.numeroCuenta = "12542369";
+cliente2.saldoCuenta = 1000;
 
-const nombreCliente = "Leonardo";
-const dniCliente = "1053249654";
-const numeroCuenta = "12542369";
-const saldoCuenta = 1000;
+console.log(cliente2);
+
+const cliente3 = new Cliente();
+cliente3.nombreCliente = "Maria";
+cliente3.dniCliente = "154662";
+cliente3.numeroCuenta = "20369";
+cliente3.saldoCuenta = 500;
+
+console.log(cliente3);
