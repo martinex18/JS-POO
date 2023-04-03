0f0ef9f55a2fb38030f7bac3712c01f85fb4bb53
Gabriel Martinez <martinegabriel47@gmail.com>
2023-04-03 13:00:53 -0500

    Create cuentaCorriente.js

---
+++ aula-4/cuentaCorriente.js
@@ -0,0 +1,36 @@
+export class cuentaCorriente{
+    cliente;
+    numero;
+    #saldo;
+    agencia;
+
+    constructor(){
+        //this.cliente = null;
+        this.numero = " ";
+        this.#saldo = 0;
+        this.agencia = " ";
+    }
+
+    depositoCuenta(valor){
+        if (valor > 0){
+            this.#saldo += valor;
+        }
+        return this.#saldo;
+    }
+
+    retirarcuenta(valor){
+        if (valor <= this.#saldo){
+            this.#saldo -= valor;
+        }
+        return this.#saldo;
+    }
+
+    verSaldo(){
+        return this.#saldo;
+    }
+
+    trasnsferirParaCuenta(valor, cuentaDestino){
+        this.retirarcuenta(valor);
+        cuentaDestino.depositoCuenta(valor);
+    }
+}
