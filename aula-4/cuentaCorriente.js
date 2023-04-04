8c8e7088b0a285b80fbb337dd4cc97a47380e28b
Gabriel Martinez <martinegabriel47@gmail.com>
2023-04-03 21:15:19 -0500

    Update cuentaCorriente.js

--- aula-4/cuentaCorriente.js
+++ aula-4/cuentaCorriente.js
@@ -1,11 +1,22 @@
-export class cuentaCorriente{
-    cliente;
+import { Cliente } from "./cliente.js";
+ export class cuentaCorriente{
+    #cliente;
     numero;
     #saldo;
     agencia;
 
+    set cliente(valor){
+        if (valor instanceof Cliente){ //Si valor es una instancia de la clase cliente
+            this.#cliente = valor;
+        }
+    }
+
+    get cliente(){
+        return this.#cliente;
+    }
+
     constructor(){
-        //this.cliente = null;
+        this.#cliente = null;
         this.numero = " ";
         this.#saldo = 0;
         this.agencia = " ";
